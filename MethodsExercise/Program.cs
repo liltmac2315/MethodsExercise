15bba34814ecc09c53abbb4fd8fa8f70905fdb6a
tommy santos <tommy1santos@yahoo.com>
2023-08-21 20:57:26 -0400

    Began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //_____________Ecercise 1___________________
+            //Name: Tommy
+            //Favorite color: Green
+            //Favorite animal: Tiger
+            //Favorite band: Beatles
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}- What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("great! now, What is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userName}! You're favorite color {color}, and you love {animal}'s , and the band {band}");
+
+
+
         }
     }
 }
